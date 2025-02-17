c73dca8599097077c6b8a685593a94269f6d96d7
Hamza El Ghdayr <hael-ghd@e3r3p11.1337.ma>
2025-02-17 19:52:37 +0100

    17/02/2025 19:15

--- bonus_part/inc/macros.h
+++ bonus_part/inc/macros.h
@@ -6,7 +6,7 @@
 /*   By: hael-ghd <hael-ghd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 17:51:22 by hael-ghd          #+#    #+#             */
-/*   Updated: 2025/02/09 16:09:46 by hael-ghd         ###   ########.fr       */
+/*   Updated: 2025/02/17 19:24:58 by hael-ghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include <fcntl.h>
 # include <string.h>
 # include <math.h>
+# include <pthread.h>
 
 # define BUFFER_SIZE 40
 
