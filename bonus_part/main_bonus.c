3636347dbf6f23aee95f74a6f3e799578014b8f9
Hamza El Ghdayr <hael-ghd@e2r2p7.1337.ma>
2025-04-12 23:10:46 +0100

    RT finish

--- bonus_part/main_bonus.c
+++ bonus_part/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hael-ghd <hael-ghd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:44:53 by hael-ghd          #+#    #+#             */
-/*   Updated: 2025/03/23 00:37:51 by hael-ghd         ###   ########.fr       */
+/*   Updated: 2025/04/12 23:09:11 by hael-ghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,33 +30,24 @@ static int	check_extention(t_scene *scene, const char *str)
 	return (close(fd), EXIT_FAILURE);
 }
 
-static t_scene	*init_struct(void)
+static void	init_struct(t_scene *scene)
 {
-	t_scene	*scene;
-
-	scene = malloc(sizeof(t_scene));
-	if (!scene)
-		return (NULL);
 	ft_memset(scene, 0, sizeof(t_scene));
 	scene->tmp_heap = ft_malloc(scene, sizeof(t_tmp_heap));
 	ft_memset(scene->tmp_heap, 0, sizeof(t_tmp_heap));
 	scene->tmp_heap->fd = -1;
 	scene->mlx.init = false;
-	return (scene);
 }
 
 int	main(int ac, char **av)
 {
-	t_scene	*scene;
+	t_scene	scene;
 
-	scene = init_struct();
-	if (!scene)
-		return (write(2, F_MALL, \
-			ft_strlen(F_MALL)), 1);
+	init_struct(&scene);
 	if (ac != 2)
-		return (print_scene_err(scene, "Invalid argument\n"), 1);
-	if (check_extention(scene, av[1]))
-		return (print_scene_err(scene, "invalid extention\n"), 1);
-	parse_part(scene, av[1]);
-	render(scene);
+		return (print_scene_err(&scene, "Invalid argument\n"), 1);
+	if (check_extention(&scene, av[1]))
+		return (print_scene_err(&scene, "invalid extention\n"), 1);
+	parse_part(&scene, av[1]);
+	render(&scene);
 }
